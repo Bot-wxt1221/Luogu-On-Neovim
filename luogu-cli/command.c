85fd61ebed3e0f2ed5b1e3f998e3b465da228949
wxt1221 <3264117476@qq.com>
2024-02-02 20:45:24 +0800

    print compile time

--- luogu-cli/command.c
+++ luogu-cli/command.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-
+#include "compile.h"
 int help(){
   printf("Luogu CLI written by Bot-wxt1221\n");
-  printf("Thanks Vscode-luogu. It provides me how to communicate with luogu server.");
+  printf("Thanks Vscode-luogu. It provides me how to communicate with luogu server.\n");
+  printf("Built on %s",TIMESTAMP);
   return 0;
 }
